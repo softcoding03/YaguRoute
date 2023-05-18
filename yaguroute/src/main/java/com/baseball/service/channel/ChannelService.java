376c92a79b473a20dcdb7d423d69eb1094745eff
Bitcamp <Bitcamp@DESKTOP-IR25RL3>
2023-05-18 21:38:17 +0900

    [jeongho] 2023.05.18 21:37 CREATE : Channel 도메인 생성, ChannelService, ChannelDao, channelRestService 생성 완료

---
+++ yaguroute/src/main/java/com/baseball/service/channel/ChannelService.java
@@ -0,0 +1,14 @@
+package com.baseball.service.channel;
+
+import java.util.List;
+
+import com.baseball.service.domain.Channel;
+
+public interface ChannelService {
+	
+	public void addChannel(Channel channel) throws Exception;
+	public Channel getChannel(String channelID) throws Exception;
+	public List<Object> getChannel() throws Exception;
+	public void updateChannel(Channel channel) throws Exception;
+	public void deleteChannel(String channelID) throws Exception;
+}
