0f08112e75fdfe7db468d6e1ef680ecfbf5a3ac2
Bitcamp <Bitcamp@DESKTOP-KESVINK>
2023-05-18 14:50:09 +0900

    [master] 2023.05.18. 14:49 CREATE: com.baseball 자바 패키지 생성

---
+++ yaguroute/src/main/java/com/baseball/YagurouteApplication.java
@@ -0,0 +1,13 @@
+package com.baseball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YagurouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YagurouteApplication.class, args);
+	}
+
+}
