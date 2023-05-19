5682878e152c5f9ab0176231121e214b380cb90c
Bitcamp <Bitcamp@DESKTOP-KESVINK>
2023-05-19 10:39:21 +0900

    [kokoku] 2023.05.19 10:38 CREATE : TeamMapper 생성 FEAT : MyBatis에 TeamMapper 등록함.

--- yaguroute/src/main/java/com/baseball/service/user/UserService.java
+++ yaguroute/src/main/java/com/baseball/service/user/UserService.java
@@ -1,6 +1,12 @@
 ﻿package com.baseball.service.user;
 
+import com.baseball.service.domain.User;
 
 //==> 회원관리에서 서비스할 내용 추상화/캡슐화한 Service  Interface Definition  
 public interface UserService {
+	
+	public void addUser(User user) throws Exception;
+	public User getUser(String userId) throws Exception;
+	public void updateUser(User user) throws Exception;
+	
 }
--- yaguroute/src/main/java/com/baseball/service/user/impl/UserServiceImpl.java
+++ yaguroute/src/main/java/com/baseball/service/user/impl/UserServiceImpl.java
@@ -1,7 +1,26 @@
 ﻿package com.baseball.service.user.impl;
 
+import com.baseball.service.domain.User;
 import com.baseball.service.user.UserService;
 
 public class UserServiceImpl implements UserService{
+
+	@Override
+	public void addUser(User user) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public User getUser(String userId) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateUser(User user) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
