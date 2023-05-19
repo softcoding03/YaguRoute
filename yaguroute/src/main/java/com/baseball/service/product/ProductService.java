6003fc17c29be911f9d644d35a066d18d74373c7
algud <algud@DESKTOP-UMDH5JP>
2023-05-19 13:28:34 +0900

    [algudgod] 2023.05.19 13:30  - CREATE: product-transaction-trandetail mapper 수정 (doctype 추가)

--- yaguroute/src/main/java/com/baseball/service/product/ProductService.java
+++ yaguroute/src/main/java/com/baseball/service/product/ProductService.java
@@ -1,5 +1,12 @@
 package com.baseball.service.product;
 
-public class ProductService {
+import com.baseball.service.domain.Product;
 
+
+
+public interface ProductService {
+
+	public void addProduct(Product product) throws Exception;
+	
+	
 }
