e987429fcdbb5ba6fd1f294977bcee92c479aeaa
Bitcamp <Bitcamp@DESKTOP-KESVINK>
2023-05-19 11:09:23 +0900

    [kokoku] 2023.05.19 11:09

--- yaguroute/src/main/java/com/baseball/service/user/UserDao.java
+++ yaguroute/src/main/java/com/baseball/service/user/UserDao.java
@@ -5,6 +5,8 @@ import com.baseball.service.domain.User;
 public interface UserDao {
 	
 	public void addUser(User user)throws Exception;
+	
 	public User getUser(String userId) throws Exception;
+	
 	public void updateUser(User user) throws Exception;
 }
--- yaguroute/src/main/java/com/baseball/service/user/impl/UserServiceImpl.java
+++ yaguroute/src/main/java/com/baseball/service/user/impl/UserServiceImpl.java
@@ -1,10 +1,22 @@
 ﻿package com.baseball.service.user.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
 import com.baseball.service.domain.User;
+import com.baseball.service.user.UserDao;
 import com.baseball.service.user.UserService;
 
+@Service("userServiceImpl")
 public class UserServiceImpl implements UserService{
 
+	//Field
+	@Autowired
+	@Qualifier("userDao")
+	private UserDao userDao;
+	
+	
 	@Override
 	public void addUser(User user) throws Exception {
 		// TODO Auto-generated method stub
