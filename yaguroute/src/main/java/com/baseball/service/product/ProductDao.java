167e36e16c5398487046f9406dcd9a51abd20a91
algudgod <algudgod@gmail.com>
2023-05-19 20:01:36 +0900

    [algudgod] 2023.05.19 20:00  - productMapper (add, get, delete, update)

--- yaguroute/src/main/java/com/baseball/service/product/ProductDao.java
+++ yaguroute/src/main/java/com/baseball/service/product/ProductDao.java
@@ -1,5 +1,7 @@
 package com.baseball.service.product;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.baseball.service.domain.Product;
@@ -12,6 +14,16 @@ public interface ProductDao {
 	
 	//Add
 	public void addProduct(Product product) throws Exception;
+	//Select One
+	public Product getProduct(int prodNo) throws Exception;
+	//Update
+	public void updateProduct(Product product) throws Exception;
+	//Delete
+	public int deleteProduct(int prodNo) throws Exception;
+	
+	
+	//getList
+	public List<Product> getProductList (Product product) throws Exception;
 	
 	
 }
--- yaguroute/src/test/java/com/baseball/test/product/ProductTest.java
+++ yaguroute/src/test/java/com/baseball/test/product/ProductTest.java
@@ -1,13 +1,18 @@
 package com.baseball.test.product;
 
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
+
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import com.baseball.service.domain.Product;
 import com.baseball.service.product.ProductService;
 
+
+@SpringBootTest
 public class ProductTest {
 
 	@Autowired
@@ -15,7 +20,7 @@ public class ProductTest {
 	private ProductService productService;
 	
 	
-	@Test
+	//@Test
 	public void testAddProduct() throws Exception {
 		
 		Product product = new Product();
@@ -25,9 +30,13 @@ public class ProductTest {
 		product.setProdStock(400);
 		product.setProdCategory("잡화");
 		product.setProdImage("testImageFile");
-		product.setProdteamCode("OB");
+		product.setProdTeamCode("OB");
+		
 			
 		productService.addProduct(product);
+		product = productService.getProduct(3);
+		
+		//System.out.println("addProductTest:: "+product);
 		
 		Assert.assertEquals(3, product.getProdNo());
 		Assert.assertEquals("정수빈레고피규어", product.getProdName());
@@ -38,4 +47,73 @@ public class ProductTest {
 		Assert.assertEquals("OB", product.getProdTeamCode());
 
 	}
+	
+	//@Test
+	public void testGetProduct() throws Exception {
+		
+		Product product = new Product();
+		
+		product = productService.getProduct(2);
+		
+		//System.out.println("TestgetProuct 할 수 있다 :: "+product);
+		
+		//==> API 확인
+		Assert.assertEquals(2, product.getProdNo());
+		Assert.assertEquals("양의지 올드 유니폼", product.getProdName());
+		Assert.assertEquals(120000, product.getProdPrice());
+		Assert.assertEquals(50, product.getProdStock());
+		Assert.assertEquals("유니폼", product.getProdCategory());
+		Assert.assertEquals("yang_old_uniform.jpg", product.getProdImage());
+		Assert.assertEquals("OB", product.getProdTeamCode());
+		
+	}
+	
+	//@Test
+	public void testUpdate() throws Exception {
+		
+		Product product = productService.getProduct(1);
+		Assert.assertNotNull(product);
+		
+		Assert.assertEquals("정은원 원정 유니폼", product.getProdName());
+		Assert.assertEquals(10000, product.getProdPrice());
+		Assert.assertEquals(10, product.getProdStock());
+		Assert.assertEquals("유니폼", product.getProdCategory());
+		Assert.assertEquals("/images/sk_uniform.jpg", product.getProdImage());
+		Assert.assertEquals("HH", product.getProdTeamCode());
+		
+	
+		product.setProdName("정은원 홈 유니폼");
+		product.setProdPrice(120000);
+		product.setProdStock(50);
+		product.setProdCategory("유니폼");
+		product.setProdImage("testImageFile");
+		product.setProdTeamCode("HH");
+		
+		
+		productService.updateProduct(product);
+		
+		product = productService.getProduct(1);
+		Assert.assertNotNull(product);
+		
+		//System.out.println("testUpdate 가랏! :: "+product);
+		
+		Assert.assertEquals("정은원 홈 유니폼", product.getProdName());
+		Assert.assertEquals(120000, product.getProdPrice());
+		Assert.assertEquals(50, product.getProdStock());
+		Assert.assertEquals("유니폼", product.getProdCategory());
+		Assert.assertEquals("testImageFile", product.getProdImage());
+		Assert.assertEquals("HH", product.getProdTeamCode());
+	}
+	
+	//@Test
+	public void testDeleteProduct() throws Exception {
+			
+		productService.deleteProduct(3);
+			
+		System.out.println("testDelete:: ");
+		
+	}
+
+	
+	
 }
