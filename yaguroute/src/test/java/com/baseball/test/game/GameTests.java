a4ff712c829142d1504f085754acdf3698d47b4c
rakseong <chltjdfkr12_@naver.com>
2023-05-20 17:47:17 +0900

    [rakseong] 2023.05.20. 17:44 FEAT:  GameMapper 작성, GameDao 모든 메서드 작성, JUnit Test 완료.

--- yaguroute/src/test/java/com/baseball/test/game/GameTests.java
+++ yaguroute/src/test/java/com/baseball/test/game/GameTests.java
@@ -1,12 +1,9 @@
 package com.baseball.test.game;
 
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -42,6 +39,57 @@ public class GameTests {
 	}
 	
 	//@Test
+	public void getGameInfo() throws Exception{
+		
+		Game game =  gameService.getGameInfo("20230313HTHH02023");
+		System.out.println(game);
+	}
+	
+	@Test
+	public void getGameScheduleList() throws Exception{
+		
+		Map<String,String> map = new HashMap<>();
+		String date = "2023-06";
+		String teamCode = "OB";
+		List<Game> list = gameService.getGameScheduleList(date,teamCode);
+		for(Game tmp : list) {
+			System.out.println(tmp);
+		}
+	}
+		
+	//@Test
+	public void updateGameState() throws Exception{
+		String date = "2023.5.20";
+		Game game = new Game();
+		game.setGameStatusCode("0");
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd");
+		game.setGameDate(dateFormat.format(dateFormat.parse(date)));
+		System.out.println(dateFormat.format(dateFormat.parse(date)));
+		gameService.updateGameState(game);
+	}
+	
+	//@Test
+	public void addGame() throws Exception{
+		Game game = new Game();
+		game.setGameCode("20100731SKSS02010");
+		
+		Team homeTeam = new Team();
+		homeTeam.setTeamCode("SS");
+		game.setHomeTeam(homeTeam);
+		
+		Team awayTeam = new Team();
+		awayTeam.setTeamCode("SK");
+		game.setAwayTeam(awayTeam);
+		
+		game.setGameDate("7.31");
+		game.setGameTime("18:30");
+		game.setGameStatusCode("2");
+		game.setGameScore("4:8");
+		
+		gameService.addThisYearGameSchedule(game);
+	}
+	
+	//@Test
 	public void addThisYearGameSchedule() throws Exception{
 		
 		SimpleDateFormat dateFromat = new SimpleDateFormat("yyyy.MM.dd");
@@ -53,71 +101,116 @@ public class GameTests {
 		System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
 		
 		ChromeOptions ops = new ChromeOptions();
-		ops.setCapability("ignoreProtectedModeSettings", true);//Driver SetUp ( �����Ͽ��� ���� ���� ���� )
+		ops.setCapability("ignoreProtectedModeSettings", true);
 		ops.addArguments("--remote-allow-origins=*");
 		ops.addArguments("headless");
 		ops.addArguments("--no-sandbox");
 		ops.addArguments("--disable-dev-shm-usage");
 		ops.addArguments("ignore-certificate-errors");
 		
-		String url = "https://sports.news.naver.com/kbaseball/schedule/index?date=20230519&month=05&year=2015&teamCode=";
+		WebDriver orderDriver = new ChromeDriver(ops);
 		WebDriver driver = new ChromeDriver(ops);
-		driver.get(url);
 		
-		List<WebElement> monthDayCount = driver.findElements(By.cssSelector("#calendarWrap > div:not(.nogame,.nogame2)"));
-		String nowYear = driver.findElement(By.xpath("//*[@id=\"_currentYearButton\"]/em")).getText().split("[\n]")[1];
+		orderDriver.get("https://sports.news.naver.com/kbaseball/schedule/index");
 		
-		List<Game> monthGameList = new ArrayList<>();
+		orderDriver.findElement(By.xpath("//*[@id=\"_currentYearButton\"]")).click();
+		List<WebElement> yearList = orderDriver.findElements(By.cssSelector("#_yearList > li"));
 		
-		for(WebElement dayGame : monthDayCount) {
-			
-			String date = dayGame.findElement(By.cssSelector(".td_date strong")).getText();
-			
-			int state = 0;
-			
-			for(WebElement oneGame : dayGame.findElements(By.cssSelector("tr"))) {
+		orderDriver.findElement(By.xpath("//*[@id=\"_currentMonthButton\"]")).click();
+		List<WebElement> monthList = orderDriver.findElements(By.cssSelector("#_monthList > li"));
+		
+		for(WebElement year : yearList) {
+			for(WebElement mon : monthList) {
+				System.out.println(mon.getText()+year.getText()+"");
 				
-				Game game = new Game();
+				String url = "https://sports.news.naver.com/kbaseball/schedule/index?date=20230520&month="+mon.getText()+"&year="+year.getText()+"&teamCode=";
 				
-				game.setGameDate(date);
-				game.setGameYear(nowYear);
+				driver.get(url);
 				
-				if(oneGame.findElement(By.cssSelector("td:nth-child(2)")).getAttribute("class").contains("add_state") || oneGame.findElement(By.cssSelector("td:nth-child(3)")).getAttribute("class").contains("add_state")) {
-					state = 3;
-				}else {
-					if(oneGame.findElement(By.cssSelector("td a")).getAttribute("href").contains("video")) {
-						state = 2;
-					}else if(oneGame.findElement(By.cssSelector("td a")).getAttribute("href").contains("preview")){
-						state = 0;
-					}else {
-						state = 1;
+				List<WebElement> monthDayCount = driver.findElements(By.cssSelector("#calendarWrap > div:not(.nogame,.nogame2)"));
+				String nowYear = year.getText();
+				
+				for(WebElement dayGame : monthDayCount) {
+					
+					String date = dayGame.findElement(By.cssSelector(".td_date strong")).getText();
+					
+					
+					
+					int state = 0;
+					
+					for(WebElement oneGame : dayGame.findElements(By.cssSelector("tr"))) {
+						
+						Game game = new Game();
+						
+						game.setGameDate(nowYear+"."+date);
+						
+						if(oneGame.findElement(By.cssSelector("td:nth-child(3) span")).getAttribute("class").contains("cancel") 
+								|| oneGame.findElement(By.cssSelector("td:nth-child(4) span")).getAttribute("class").contains("cancel") 
+								|| oneGame.findElement(By.cssSelector("td:nth-child(2)")).getAttribute("class").contains("add_state")
+								|| oneGame.findElement(By.cssSelector("td:nth-child(3)")).getAttribute("class").contains("add_state")) {
+							state = 3;
+						}else {
+							if(oneGame.findElement(By.cssSelector("td a")).getAttribute("href").contains("record")) {
+								state = 2;
+							}else if(oneGame.findElement(By.cssSelector("td a")).getAttribute("href").contains("preview")){
+								state = 0;
+							}else {
+								state = 1;
+							}
+						}
+						game.setGameStatusCode(String.valueOf(state));
+						game.setGameTime(oneGame.findElement(By.cssSelector(".td_hour")).getText());
+						if(nowYear.equals("2021") && (date.equals("5.8") || date.equals("5.9")) && game.getGameTime().equals("14:00")) {
+							continue;
+						}
+						String teamName="";
+						if(oneGame.findElement(By.cssSelector(".team_lft")).getText().trim().equals("넥센")){
+							teamName = "키움";
+						}else if(oneGame.findElement(By.cssSelector(".team_lft")).getText().trim().equals("SK")){
+							teamName = "SSG";
+						}else if(oneGame.findElement(By.cssSelector(".team_lft")).getText().trim().equals("나눔") 
+								|| oneGame.findElement(By.cssSelector(".team_lft")).getText().trim().equals("드림")
+								|| oneGame.findElement(By.cssSelector(".team_lft")).getText().trim().equals("웨스턴")
+								|| oneGame.findElement(By.cssSelector(".team_lft")).getText().trim().equals("이스턴")
+								|| oneGame.findElement(By.cssSelector(".team_lft")).getText().trim().equals("서군")
+								|| oneGame.findElement(By.cssSelector(".team_lft")).getText().trim().equals("동군")){
+							continue;
+						}else {
+							teamName = oneGame.findElement(By.cssSelector(".team_lft")).getText().trim();
+						}
+						game.setAwayTeam(gameService.getTeamInfoByTeamName(teamName));
+						
+						if(oneGame.findElement(By.cssSelector(".team_rgt")).getText().trim().equals("넥센") || oneGame.findElement(By.cssSelector(".team_rgt")).getText().trim().equals("넥센")){
+							teamName = "키움";
+						}else if(oneGame.findElement(By.cssSelector(".team_rgt")).getText().trim().equals("SK") || oneGame.findElement(By.cssSelector(".team_rgt")).getText().trim().equals("SK")){
+							teamName = "SSG";
+						}else if(oneGame.findElement(By.cssSelector(".team_lft")).getText().trim().equals("나눔") 
+								|| oneGame.findElement(By.cssSelector(".team_lft")).getText().trim().equals("드림")
+								|| oneGame.findElement(By.cssSelector(".team_lft")).getText().trim().equals("웨스턴")
+								|| oneGame.findElement(By.cssSelector(".team_lft")).getText().trim().equals("이스턴")
+								|| oneGame.findElement(By.cssSelector(".team_lft")).getText().trim().equals("서군")
+								|| oneGame.findElement(By.cssSelector(".team_lft")).getText().trim().equals("동군")){
+							continue;
+						}else {
+							teamName = oneGame.findElement(By.cssSelector(".team_rgt")).getText().trim();
+						}
+						game.setHomeTeam(gameService.getTeamInfoByTeamName(teamName));
+						game.setGameScore(oneGame.findElement(By.cssSelector(".td_score")).getText());
+						String gameTmp = strFromat.format(dateFromat.parse(game.getGameDate()))+game.getAwayTeam().getTeamCode()+game.getHomeTeam().getTeamCode()+"0"+(Integer.parseInt(nowYear)<=2015?"":nowYear);
+						if(gameService.getGameInfo(gameTmp) != null) {
+							continue;
+						}else {
+							game.setGameCode(gameTmp);
+							gameService.addThisYearGameSchedule(game);
+						}
+						System.out.println(game);
+						
 					}
 				}
-				game.setGameStatusCode(String.valueOf(state));
-				game.setGameTime(oneGame.findElement(By.cssSelector(".td_hour")).getText());
-				String teamName="";
-				if(oneGame.findElement(By.cssSelector(".team_lft")).getText().trim().equals("넥센") || oneGame.findElement(By.cssSelector(".team_rgt")).getText().trim().equals("넥센")){
-					teamName = "키움";
-				}else if(oneGame.findElement(By.cssSelector(".team_lft")).getText().trim().equals("SK") || oneGame.findElement(By.cssSelector(".team_rgt")).getText().trim().equals("SK")){
-					teamName = "SSG";
-				}else {
-					teamName = oneGame.findElement(By.cssSelector(".team_lft")).getText().trim();
-				}
-				game.setAwayTeam(gameService.getTeamInfoByTeamName(teamName));
-				
-				if(oneGame.findElement(By.cssSelector(".team_rgt")).getText().trim().equals("넥센") || oneGame.findElement(By.cssSelector(".team_rgt")).getText().trim().equals("넥센")){
-					teamName = "키움";
-				}else if(oneGame.findElement(By.cssSelector(".team_rgt")).getText().trim().equals("SK") || oneGame.findElement(By.cssSelector(".team_rgt")).getText().trim().equals("SK")){
-					teamName = "SSG";
-				}else {
-					teamName = oneGame.findElement(By.cssSelector(".team_rgt")).getText().trim();
-				}
-				game.setHomeTeam(gameService.getTeamInfoByTeamName(teamName));
-				game.setGameScore(oneGame.findElement(By.cssSelector(".td_score")).getText());
-				game.setGameCode(strFromat.format(dateFromat.parse(nowYear+"."+game.getGameDate()))+game.getAwayTeam().getTeamCode()+game.getHomeTeam().getTeamCode()+"0"+nowYear);
-				System.out.println(game);
 			}
 		}
+		driver.close();
+		orderDriver.close();
 		
 //		List<WebElement> test = new ArrayList<>();
 //		for(int i=1; i<=monthDayCount.size();i++) {
@@ -132,7 +225,7 @@ public class GameTests {
 		
 		
 		
-		driver.quit();
+		
 	}
 
 }
