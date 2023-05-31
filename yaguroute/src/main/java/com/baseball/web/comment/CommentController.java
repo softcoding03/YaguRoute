4c8e1f343143d7206181bd399c15fe36e719c27d
softcoding03 <didtn1233@gmail.com>
2023-05-31 16:58:05 +0900

    [soo] 2023.05.31 16:58. FEAT : Comment List,수정,삭제,1,2레이어 구현 완료

--- yaguroute/src/main/java/com/baseball/web/comment/CommentController.java
+++ yaguroute/src/main/java/com/baseball/web/comment/CommentController.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.baseball.service.comment.CommentService;
 import com.baseball.service.domain.Comment;
@@ -42,7 +44,33 @@ public class CommentController {
 		comment.setUser(user);
 		System.out.println("insert되는 comment? "+comment);
 		commentService.addComment(comment);
+		int postNo = comment.getPostNo();
 		
-		return "forward:/post/get.jsp";
+		return "redirect:/post/getPost?postNo="+postNo;
 	}
+	
+	@PostMapping("updateComment")
+	public String updateComment(@ModelAttribute("comment") Comment comment, Model model,HttpSession session) throws Exception {
+		System.out.println("/comment/updateComment : POST START");	
+		System.out.println("-- 넘어온 데이터 ? "+comment); 
+		User user = (User)session.getAttribute("user");
+		comment.setUser(user);
+		System.out.println("update되는 comment? "+comment);
+		commentService.updateComment(comment);
+		int postNo = comment.getPostNo();
+		
+		return "redirect:/post/getPost?postNo="+postNo;
+	}
+	
+	@GetMapping("deleteComment")
+	public String deleteComment(@RequestParam("commentNo") int commentNo,@RequestParam("postNo") int postNo, Model model,HttpSession session) throws Exception {
+		System.out.println("/comment/deleteComment : GET START");	
+		System.out.println("-- 넘어온 데이터 ? "+commentNo+"//"+postNo); 
+		commentService.deleteComment(commentNo); //레코드가 삭제되지 않고 changed 칼럼이 2로 변경됨(0:x,1:수정됨,2:삭제됨)
+		
+		return "redirect:/post/getPost?postNo="+postNo;
+	}
+	
+	
+	
 }
